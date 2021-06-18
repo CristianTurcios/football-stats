110e01721aae8709804c2e321511ce327c8285e1
Cristian Turcios <cristian.turcios@proximitycr.com>
2021-06-18 10:59:50 -0600

    added prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    url: 'https://v3.football.api-sports.io',
+    token: 'd6d487a70fa56ad8bf5070ddbb282111',
+  }
 };
